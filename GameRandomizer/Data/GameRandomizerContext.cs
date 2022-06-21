7d680ba74936ca688051b1f0fe101a18bdad273e
Ленар Хабибуллин <leonruscraft@gmail.com>
2022-06-21 14:48:16 +0500

    Add project files.

---
+++ GameRandomizer/Data/GameRandomizerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GameRandomizer.Models;
+
+namespace GameRandomizer.Data
+{
+    public class GameRandomizerContext : DbContext
+    {
+        public GameRandomizerContext (DbContextOptions<GameRandomizerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GameRandomizer.Models.Game>? Game { get; set; }
+    }
+}
---
+++ GameRandomizer/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using GameRandomizer.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<GameRandomizerContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("GameRandomizerContext") ?? throw new InvalidOperationException("Connection string 'GameRandomizerContext' not found.")));
+
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Games}/{action=Index}/{id?}");
+
+app.Run();
